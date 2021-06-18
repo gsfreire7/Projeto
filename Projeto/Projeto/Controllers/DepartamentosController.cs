1266d28bcac46ccb12fe0997693873ddf5c7baa8
gsfreire7 <85702389+gsfreire7@users.noreply.github.com>
2021-06-18 16:35:59 -0300

    Criação do controlador Departamentos e da View (com mto custo)

---
+++ Projeto/Projeto/Controllers/DepartamentosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Projeto.Models;
+
+namespace Projeto.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> lista = new List<Departamento>();
+            lista.Add(new Departamento { Id = 1, Nome = "Jogos Eletrônicos" });
+            lista.Add(new Departamento { Id = 2, Nome = "Futebol" });
+            return View(lista);
+        }
+    }
+}

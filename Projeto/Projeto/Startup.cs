f6c78a676262801078d2fd6d88a151f3d6075aca
gsfreire7 <85702389+gsfreire7@users.noreply.github.com>
2021-06-28 11:16:12 -0300

    Projeto que já abandonei

--- Projeto/Projeto/Startup.cs
+++ Projeto/Projeto/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projeto.Data;
 
 namespace Projeto
 {
@@ -24,6 +26,10 @@ namespace Projeto
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<ProjetoContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("ProjetoContext"), builder => 
+                    builder.MigrationsAssembly("Projeto")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
